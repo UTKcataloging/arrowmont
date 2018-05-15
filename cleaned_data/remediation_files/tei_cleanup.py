8b772f1e885c34d97fd41c64eb693e046bbf4ef5
Mark Baggett <mbagget1@utk.edu>
2018-05-15 11:04:52 -0400

    Modify cleanup script for letters.

--- cleaned_data/remediation_files/tei_cleanup.py
+++ cleaned_data/remediation_files/tei_cleanup.py
@@ -1,10 +1,14 @@
 import os
 
-path = "/home/mark/metadata/Arrowmont/original_data/TEI/Arrow_Articles"
+path = "/home/mark/metadata/Arrowmont/original_data/TEI/Letters_TEI"
 for x in os.walk(path):
     for file in x[2]:
         with open(f"{path}/{file}", "r") as input:
             with open(f"{path}/output/{file}", "w") as output:
                 for line in input:
-                    if line != '<!DOCTYPE TEI.2 SYSTEM "tei.dtd">\n':
+                    if line == '<?xml-stylesheet type="xml/xslt" href="../../frameworks/tei/xsl/html/teihtml-teic.xsl"?>\n':
+                        pass
+                    elif line.startswith('<!DOCTYPE TEI.2 SYSTEM '):
+                        pass
+                    else:
                         output.write(line)
