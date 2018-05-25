537636ffe82b5ff780f1987bdc2968560e8a7e16
Mark Baggett <mbagget1@utk.edu>
2018-05-25 12:46:32 -0400

    Add title contatenation.

--- cleaned_data/remediation_files/compound_objects.py
+++ cleaned_data/remediation_files/compound_objects.py
@@ -2,8 +2,8 @@ import os
 import csv
 
 def main():
-    output = "/home/mark/metadata/Arrowmont/cleaned_data/mods/3dobject-compound_objects"
-    path_to_objects = "/gwork/mark/metadata/Arrowmont/original_data/MODS/Arrow_MODS"
+    output = "/gwork/mbagget1/metadata/arrowmont/cleaned_data/mods/3dobject-compound_objects"
+    path_to_objects = "/gwork/mbagget1/360_archival_tiff/arbuckle_tiff/"
     for path in os.walk(path_to_objects):
         for object in path[2]:
             new_Record = Record(object, output)
@@ -16,7 +16,7 @@ class Record:
         self.output = destination
 
     def find_matching_row(self):
-        my_csv = "/home/mark/metadata/Arrowmont/original_data/csvs/3dobject-videos.csv"
+        my_csv = "/gwork/mbagget1/metadata/arrowmont/original_data/csvs/3dobject-videos.csv"
         with open(my_csv) as csv_file:
             readCSV = csv.reader(csv_file, delimiter=',')
             for row in readCSV:
@@ -26,9 +26,10 @@ class Record:
 
     def create_metadata_record(self):
         output_file = self.filename.replace("TIF", "xml")
-        root = '<mods xmlns="http://www.loc.gov/mods/v3" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="3.5" xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-5.xsd">'
+        title_addition = self.filename.replace(".TIF", "")
+        root = '<mods xmlns="http://www.loc.gov/mods/v3" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="3.5" xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-5.xsd">\n'
         creator = f"<name><namePart>{self.data[0]}</namePart><role><roleTerm>Creator</roleTerm></role></name>"
-        title = f"<titleInfo><title>{self.data[1]}</title></titleInfo>"
+        title = f"<titleInfo><title>{self.data[1]} - part {title_addition}</title></titleInfo>"
         origin = f"<originInfo><dateCreated>{self.data[2]}</dateCreated></originInfo>"
         physical = f"<physicalDescription><extent>{self.data[3]}</extent></physicalDescription>"
         abstract = f"<abstract>{self.data[4]}</abstract>"
@@ -37,7 +38,7 @@ class Record:
         record_info = '<recordInfo><recordContentSource valueURI="http://id.loc.gov/authorities/names/n87808088">University of Tennessee, Knoxville. Libraries</recordContentSource></recordInfo>'
         type_of_resource = '<typeOfResource>three dimensional object</typeOfResource>'
         with open(f"{self.output}/{output_file}", "w") as final_xml:
-            final_xml.write('<?xml version="1.0" encoding="UTF-8"?>')
+            final_xml.write('<?xml version="1.0" encoding="UTF-8"?>\n')
             final_xml.write(root)
             final_xml.write(title)
             final_xml.write(creator)
